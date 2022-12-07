3fa4f55791b368ca9b8b7631366923bbe87a609a
Jay Cheskis <jay.cheskis@gmail.com>
2022-12-07 12:18:33 -0600

    cleaned up react.tsx

--- src/react.tsx
+++ src/react.tsx
@@ -1,4 +1,3 @@
-// src/react.tsx
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
  
@@ -7,23 +6,3 @@ const Index = () => {
 };
  
 ReactDOM.render(<Index />, document.getElementById('app'));
-
-
-/*
-import { HashRouter } from 'react-router-dom';
- DO WE NEED TO IMPORT THIS
-i think so lol but idk cause they didn't show them making it in theirs
-article or video
-article
-import React from 'react';
-import { render } from 'react-dom';
-*/
-
-/*
-render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
-  document.getElementById('root')
-)
-*/
