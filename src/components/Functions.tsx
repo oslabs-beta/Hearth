087e926d9394b533d7f29add6e3df08d24aeed12
Linda Ngo <110499774+lindango142@users.noreply.github.com>
2022-12-23 16:14:00 -0800

    Added nav bar and function pages
    Co-authored-by: Christina Phung <phung.christina16@gmail.com>
    Co-authored-by: Andy Oh <andyoh.94@gmail.com>

--- src/components/Functions.tsx
+++ src/components/Functions.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { Box, Button, Divider, Drawer, Typography, useMediaQuery, ListItem, ListItemButton, ListItemText, List } from '@mui/material';
 import FunctionDetails from './FunctionDetails';
+import axios from 'axios';
+import { useActionData } from 'react-router';
 
 const Functions = (props) => {
 const [showSlider, setShowSlider] = useState(false);
+const [colorStay, setColor] = useState('white');
+const [lastInvoke, setLastInvoke] = useState('');
+const [lastInvokeDate, setLastInvokeDate] = useState('');
+const [data, setData] = useState('')
+// let passData;
 
 const slider = [];
 if (showSlider) {
@@ -13,14 +20,35 @@ if (showSlider) {
 
 const handleClick = () => {
   setShowSlider(!showSlider)
+  if (colorStay === 'white') setColor('#E3F2FD')
+  else setColor('white')
+  props.handleDataChange(data);
+  console.log(data);
 }
 
 
+useEffect(() => {
+  axios.get('http://localhost:3000/cloud/logs', { params: { funcName: props.Name, externalId: props.externalId, arn: props.arn, region: props.region }})
+    .then((data) => {
+      console.log(data);
+      setLastInvoke(data.data[data.data.length - 1].Time);
+      setLastInvokeDate(data.data[data.data.length - 1].Date)
+      // passData = data.data
+      setData(data.data);
+    })
+    .catch((err) => console.log(`Error: ${err}`))
+}, [])
+
+
 return (
-  <Box sx={{ width:'100%'}}>
+  <Box sx={{ width:'80%'}}>
     {/* <Button variant='text'sx={{width: '100%', maxWidth:300}}>  */}
-      <ListItemButton onClick={handleClick}> 
+      <ListItemButton onClick={handleClick} sx={{display:'flex', flexDirection: 'column', pl: 4 , borderRadius: 2, "&:hover":{background:'#E3F2FD'}, /*"&:focus":{background:'#E3F2FD'}*/"&:active":{background:"#90CAF9"}, background: colorStay}}> 
         <ListItemText primary={props.Name} sx={{fontWeight:'bold', width:'100%'}}></ListItemText>
+        <Box>
+          <ListItemText primary={lastInvoke} sx={{fontWeight:'bold', width:'100%'}}></ListItemText>
+          <ListItemText primary={lastInvokeDate} sx={{fontWeight:'bold', width:'100%'}}></ListItemText>
+        </Box>
       </ListItemButton>
     {/* </Button> */}
     {/* <Box> */}
