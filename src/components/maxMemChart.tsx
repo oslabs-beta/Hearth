b5d33b78f822501ef18bdec767de2e1914ca61b0
Andy <andyoh.94@gmail.com>
2022-12-28 16:53:58 -0800

    starting maxMemUsed chart

---
+++ src/components/maxMemChart.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { useState, useEffect, useRef } from 'react'
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+
+const MaxMem = (props) => {
+  const options = {
+    type: 'line',
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+        display: false
+      },
+      title: {
+        display: true,
+        text: 'Max Memory Used',
+        font: {
+          size: 25
+        }
+      }
+    },
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Invocations (most recent)'
+        }
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'Megabytes (MB)'
+        }
+      }
+    }
+  };
+  const labels = [1, 2, 3, 4, 5, 6, 7, 8, 9 , 10];
+
+  const invocationMaxMem = [];
+  
+}
