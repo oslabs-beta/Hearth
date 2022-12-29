929fb5946898e921c9fdda4c8f01d3d0afb09343
christinaaphungg <110958807+christinaaphungg@users.noreply.github.com>
2022-12-28 23:12:41 -0800

    Update LogTable.tsx
    removed unnecessary data

--- src/components/LogTable.tsx
+++ src/components/LogTable.tsx
@@ -61,27 +61,7 @@ function createData(
 ): Data {
   return { date, duration, billedDuration, maxMemUsed, initDuration };
 }
-/*
-const rows = [
-  
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
-  
-];
-*/
+
 export default function LogTable(props) {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
