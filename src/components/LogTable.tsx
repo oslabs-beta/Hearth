3769d37d123daf4a7645a7278c50ef25a5a5c4db
Linda Ngo <110499774+lindango142@users.noreply.github.com>
2022-12-29 17:51:01 -0800

    added tabs to charts and fixed logs sizing

--- src/components/LogTable.tsx
+++ src/components/LogTable.tsx
@@ -113,7 +113,7 @@ export default function LogTable(props) {
   }, [props.logData]);
   return (
     <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
+      <TableContainer sx={{ maxHeight: 230 }}>
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
             <TableRow>
